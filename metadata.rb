43fe63955317a23e8019f65876ecc92529954fb5
Dan Poggi <dan.poggi@gmail.com>
2014-07-02 14:30:57 -0400

    Depend on users::sysadmins before running

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,12 @@ maintainer       'Dan Poggi'
 maintainer_email 'dan.poggi@gmail.com'
 license          'MIT'
 description      'Installs/configures dotfiles and other configuration standards'
-version          '0.1.3'
+version          '0.1.4'
 
 recipe 'dcp', 'Installs/configures dotfiles and other configuration standards'
 
 depends 'git'
 depends 'zsh'
+depends 'users'
 
 supports 'ubuntu'
